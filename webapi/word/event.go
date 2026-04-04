5f00e9088843e24848c325beb96dbddc33630360
stryzhka <voditel.vlad@yandex.ru>
2026-04-04 16:16:41 +0300

    fix

--- webapi/word/event.go
+++ webapi/word/event.go
@@ -3,10 +3,15 @@ package word
 import "context"
 
 type WordConfirmMessage struct {
-	UserId string `bson:"user_id" json:"user_id"`
+	UserId   string `bson:"user_id" json:"user_id"`
+	ObjectId string `bson:"object_id" json:"object_id"`
+}
+
+type WordConfirmedSuccessMessage struct {
 }
 
 type EventPublisher interface {
 	Publish(ctx context.Context, topic, key string, payload any) error
+
 	Close() error
 }
